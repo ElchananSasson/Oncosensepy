aca6b020e904f53513afe8c8b0cc8a8b6b66583a
elchanans <elchanans123@gmail.com>
2023-05-22 12:45:01 +0300

    update main with all the arguments

--- main.py
+++ main.py
@@ -6,27 +6,25 @@ if __name__ == '__main__':
     data_set_path = r'Data/' + data_name + '.xlsx'
 
     l_df, g_df = osp.get_LG_data(data_set_path)
+
     err_limit_lambda = pd.read_excel(data_set_path, sheet_name='ErrorLimitLambda').columns.values[0]
 
-    important_l = osp.important_L(l_df, err_limit_lambda, 2)
-    # osp.create_new_sheet(important_l, data_set_path, 'important_L')
+    important_l = osp.important_L(l_df, err_limit_lambda, 2, new_sheet=False, sheet_name='important_L',
+                                  path=data_set_path)
 
-    # filter_dosage = osp.filter_by_col(important_l, 'dosage', ['0.00001nm', '1nm', '40nm', '1uM'])
-    # osp.create_new_sheet(filter_dosage, data_set_path, 'filter_by_dosage')
-
-    # filter_time = osp.filter_by_col(filter_dosage, 'time', ['0hr', '24hr'])
-    # osp.create_new_sheet(filter_time, data_set_path, 'filter_by_dosage_time')
-
-    # important_g = osp.sort_G_values(g_df, important_l.columns[6:], '', save=False)
-    # osp.create_new_sheet(important_g, data_set_path, 'Sorted_G')
-
-    # pairs_dict = osp.pairs_df_to_dict(important_l, 'MDAMB468', fixed_col='time')  # data27
-    pairs_dict = osp.pairs_df_to_dict(important_l, 'MCF7', fixed_col='time')  # data66
-    # pairs_dict = osp.pairs_df_to_dict(important_l, 'MCAS', fixed_col='time')  # data69
-
-    analyzed_pairs_dict = osp.analyze_pairs(pairs_dict, 0.05, display=False, only_avg=True)
-    pairs_df = osp.create_pairs_df(analyzed_pairs_dict)
-    osp.create_new_sheet(pairs_df, data_set_path, 'MCF7')
+    # filter_dosage = osp.filter_by_col(important_l, 'dosage', ['0.00001nm', '1nm', '40nm', '1uM'], new_sheet=False,
+    #                                   sheet_name='important_L', path=data_set_path)
     #
+    # filter_time = osp.filter_by_col(filter_dosage, 'time', ['0hr', '24hr'], new_sheet=False, sheet_name='important_L',
+    #                                 path=data_set_path)
+    #
+    # important_g = osp.sort_G_values(g_df, important_l.columns[6:], save_plot_path='', save=False, new_sheet=False,
+    #                                 sheet_name='Sorted_G', data_path=data_set_path)
+
+    # Aviv - fix that function
+    analyzed_pairs_dict = osp.analyze_pairs(important_l, ['MCF7', 'SKBR3-MCH'], fixed_col='time', p_value=0.05,
+                                            only_avg=True, data_path=data_set_path)
+
+    # Elchanan - continue from here
     # control_treatment_df = osp.analyze_control_treatment(important_l, 'MCF7')
     # osp.create_new_sheet(control_treatment_df, data_set_path, 'MCF7_control_vs_treatment')
